c14edf89426ff9c7c07b62b3770e0eba5e2bd35f
KBeKind <79383539+KBeKind@users.noreply.github.com>
2023-04-20 20:17:22 -0500

    finished more stuff for studio cleaned up etc

--- src/main/java/org/launchcode/skillstracker/controllers/SkillsController.java
+++ src/main/java/org/launchcode/skillstracker/controllers/SkillsController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class SkillsController {
 
     @GetMapping("/")
-    public String displayProgramLanguages(){
+    public String displayProgramLanguages() {
         return "<html>" +
                 "<body>" +
                 "<h1>Skills Tracker</h1>" +
@@ -28,37 +28,62 @@ public class SkillsController {
     }
 
     @GetMapping("/form")
-    public String displayForm(){
+    public String displayForm() {
         return "<html>" +
                 "<body>" +
                 "<h1>Choose Your Favorite!</h1>" +
                 "<form action='/favoriteDisplay'>" +
-                    "<input type='text' name='name'>" +
-                    "<select name='favorite1'>" +
-                        "<option value='Java'>Java</option>" +
-                        "<option value='JavaScript'>JavaScript</option>" +
-                        "<option value='Rust'>Rust</option>" +
-                        "<option value='Python'>Python</option>" +
-                    "</select>" +
-                    "<select name='favorite2'>" +
-                        "<option value='Java'>Java</option>" +
-                        "<option value='JavaScript'>JavaScript</option>" +
-                        "<option value='Rust'>Rust</option>" +
-                        "<option value='Python'>Python</option>" +
-                    "</select>" +
-                    "<select name='favorite3'>" +
-                        "<option value='Java'>Java</option>" +
-                        "<option value='JavaScript'>JavaScript</option>" +
-                        "<option value='Rust'>Rust</option>" +
-                        "<option value='Python'>Python</option>" +
-                    "</select>" +
-                    "<select name='favorite4'>" +
-                        "<option value='Java'>Java</option>" +
-                        "<option value='JavaScript'>JavaScript</option>" +
-                        "<option value='Rust'>Rust</option>" +
-                        "<option value='Python'>Python</option>" +
-                    "</select>" +
-                    "<button type='submit'>Submit</button>" +
+                "<label>Name</label>" +
+                "<br>" +
+                "<input type='text' name='name'>" +
+                "<br>" +
+                "<br>" +
+                "<label>Favorite</label>" +
+                "<br>" +
+                "<select name='favorite1'>" +
+                "<option></option>" +
+                "<option value='Java'>Java</option>" +
+                "<option value='JavaScript'>JavaScript</option>" +
+                "<option value='Rust'>Rust</option>" +
+                "<option value='Python'>Python</option>" +
+                "</select>" +
+                "<br>" +
+                "<br>" +
+                "<label>Second Favorite</label>" +
+                "<br>" +
+                "<select name='favorite2'>" +
+                "<option></option>" +
+                "<option value='Java'>Java</option>" +
+                "<option value='JavaScript'>JavaScript</option>" +
+                "<option value='Rust'>Rust</option>" +
+                "<option value='Python'>Python</option>" +
+                "</select>" +
+                "<br>" +
+                "<br>" +
+                "<label>Third Favorite</label>" +
+                "<br>" +
+                "<select name='favorite3'>" +
+                "<option></option>" +
+                "<option value='Java'>Java</option>" +
+                "<option value='JavaScript'>JavaScript</option>" +
+                "<option value='Rust'>Rust</option>" +
+                "<option value='Python'>Python</option>" +
+                "</select>" +
+                "<br>" +
+                "<br>" +
+                "<label>Fourth Favorite</label>" +
+                "<br>" +
+                "<select name='favorite4'>" +
+
+                "<option></option>" +
+                "<option value='Java'>Java</option>" +
+                "<option value='JavaScript'>JavaScript</option>" +
+                "<option value='Rust'>Rust</option>" +
+                "<option value='Python'>Python</option>" +
+                "</select>" +
+                "<br>" +
+                "<br>" +
+                "<button type='submit'>Submit</button>" +
                 "</form>" +
                 "</body>" +
                 "</html>";
@@ -81,10 +106,10 @@ public class SkillsController {
                 "</thead>" +
                 "<tbody>" +
                 "<tr>" +
-                    "<td>" + favorite1 + "</td>" +
-                    "<td>" + favorite2 + "</td>" +
-                    "<td>" + favorite3 + "</td>" +
-                    "<td>" + favorite4 + "</td>" +
+                "<td>" + favorite1 + "</td>" +
+                "<td>" + favorite2 + "</td>" +
+                "<td>" + favorite3 + "</td>" +
+                "<td>" + favorite4 + "</td>" +
                 "</tr>" +
                 "</tbody>" +
                 "</table>" +
